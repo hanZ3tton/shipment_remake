55b0dfa9339495ac1d55db0c89c341ede674bf78
ihsaniga <ihsannsrllh@gmail.com>
2025-08-11 11:13:56 +0700

    add Auth controller

---
+++ application/controllers/Auth.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Load necessary libraries, models, or helpers here
+        $this->load->library('session');
+        $this->load->model('Auth_model'); // Assuming you have a User_model for user operations
+        $this->load->library('form_validation');
+        $this->load->database();
+    }
+
+
+}
