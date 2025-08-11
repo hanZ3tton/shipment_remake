3e32e0f04fedb71a44c867e62e66eebf7f88a6e3
Sayntao <119290666+Sayntao@users.noreply.github.com>
2025-08-11 11:55:12 +0700

    sample controller

---
+++ application/controllers/Sample.php
@@ -0,0 +1,24 @@
+<?php
+class Sample extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Load any necessary models, libraries, or helpers here
+    }
+
+    public function index()
+    {
+        // This method will be called when the controller is accessed
+        $data['title'] = 'Sample Page';
+        $data['content'] = 'page/home';
+        $this->load->view('app.php', $data);
+    }
+
+    public function another_method()
+    {
+        // Another method for demonstration
+        echo "This is another method in the Sample controller.";
+    }
+}
---
+++ application/views/app.php
@@ -0,0 +1,3 @@
+<?php $this->load->view('layout/head', $title) ?>
+<?php $this->load->view($content) ?>      
+<?php $this->load->view('layout/script') ?>
