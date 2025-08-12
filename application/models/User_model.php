4a3332adb26045315f8a7c06ea13c6fd0f5b363b
ihsaniga <ihsannsrllh@gmail.com>
2025-08-12 16:10:01 +0700

    s in users

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -40,6 +40,6 @@ class User_model extends CI_Model
      */
     public function register($data)
     {
-        return $this->db->insert('user', $data);
+        return $this->db->insert('users', $data);
     }
 }
