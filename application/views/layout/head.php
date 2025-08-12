eb59192c7e716018f8969535784169ead3f4658d
hanZ3tton <zidane.kr@icloud.com>
2025-08-12 10:24:02 +0700

    fixing

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -8,13 +8,13 @@
     <title><?= $title ?></title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url("assets/vendor/fontawesome-free/css/all.min.css") ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url("assets/css/sb-admin-2.min.css") ?> " rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
 </head>
