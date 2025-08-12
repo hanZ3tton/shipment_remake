5de80c8b18af7d30c133d6e0b95da6e6e014a9db
ihsaniga <ihsannsrllh@gmail.com>
2025-08-12 18:56:25 +0700

    nn

---
+++ application/controllers/app/Profile.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('User_model', 'profile');
+    }
+
+    // Read data
+    public function index()
+    {
+        $data['title'] = 'Profile';
+        $data['users'] = $this->profile->get_all(); // ambil semua user
+        $data['content'] = 'app/user/index'; // This should point to the profile view
+        $this->load->view('app', $data);
+    }
+
+    // Create data
+    public function create()
+    {
+        $data = [
+            'user_name'     => $this->input->post('user_name'),
+            'password'      => password_hash($this->input->post('password'), PASSWORD_BCRYPT),
+            'full_name'     => $this->input->post('full_name'),
+            'date_of_birth' => $this->input->post('date_of_birth'),
+            'email'         => $this->input->post('email'),
+            'phone_number'  => $this->input->post('phone_number'),
+            'state'         => $this->input->post('state'),
+            'city'          => $this->input->post('city'),
+            'address'       => $this->input->post('address'),
+            'created_at'    => date('Y-m-d H:i:s'),
+            'updated_at'    => date('Y-m-d H:i:s')
+        ];
+
+        $this->profile->insert($data);
+        redirect('profile');
+    }
+
+    // Update data
+    public function update($id)
+    {
+        $data = [
+            'user_name'     => $this->input->post('user_name'),
+            'full_name'     => $this->input->post('full_name'),
+            'date_of_birth' => $this->input->post('date_of_birth'),
+            'email'         => $this->input->post('email'),
+            'phone_number'  => $this->input->post('phone_number'),
+            'state'         => $this->input->post('state'),
+            'city'          => $this->input->post('city'),
+            'address'       => $this->input->post('address'),
+            'updated_at'    => date('Y-m-d H:i:s')
+        ];
+
+        if (!empty($this->input->post('password'))) {
+            $data['password'] = password_hash($this->input->post('password'), PASSWORD_BCRYPT);
+        }
+
+        $this->profile->update($id, $data);
+        redirect('profile');
+    }
+
+    // Delete multiple data
+    public function delete()
+    {
+        $ids = $this->input->post('ids');
+        if (!empty($ids)) {
+            foreach ($ids as $id) {
+                $this->profile->delete($id);
+            }
+        }
+        redirect('profile');
+    }
+}
